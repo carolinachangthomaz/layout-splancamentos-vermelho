33bbcaf319a68f8d92dedaa6904762679da3ec68
Carolina Chang <carol.com.jp@gmail.com>
2020-06-06 19:48:08 -0300

    Projeto - add imagens planta

--- src/app/common/projeto/projeto.component.ts
+++ src/app/common/projeto/projeto.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projeto.component.css']
 })
 export class ProjetoComponent implements OnInit {
+  public planta_trinta_m;
+  public planta_sessenta_m;
 
   constructor() { }
 
   ngOnInit() {
+    this.planta_trinta_m = '../../../assets/imagens/lancamento-neohouse-campo-belo-planta-30,16m2-1.webp';
+    this.planta_sessenta_m = '../../../assets/imagens/lancamento-neohouse-campo-belo-planta-65,52m2-2.webp';
   }
 
 }
