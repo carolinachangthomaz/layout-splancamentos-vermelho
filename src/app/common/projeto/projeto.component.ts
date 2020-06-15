ac42735cd2201f497b6ec50d73fb28b5db165d6d
Carolina Chang <carol.com.jp@gmail.com>
2020-06-14 22:13:11 -0300

    Projeto - Plantas - troca de imagem e alteração no tamanho

--- src/app/common/projeto/projeto.component.ts
+++ src/app/common/projeto/projeto.component.ts
@@ -7,13 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjetoComponent implements OnInit {
   public planta_trinta_m;
+  public planta_quarente_m;
   public planta_sessenta_m;
 
   constructor() { }
 
   ngOnInit() {
-    this.planta_trinta_m = '../../../assets/imagens/lancamento-neohouse-campo-belo-planta-30,16m2-1.webp';
-    this.planta_sessenta_m = '../../../assets/imagens/lancamento-neohouse-campo-belo-planta-65,52m2-2.webp';
+    this.planta_trinta_m = '../../../assets/imagens/planta-30.jpg';
+    this.planta_quarente_m = '../../../assets/imagens/planta-46.jpg';
+    this.planta_sessenta_m = '../../../assets/imagens/planta-65.jpg';
   }
 
 }
